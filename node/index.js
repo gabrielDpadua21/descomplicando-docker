c4b5ac194894ca868294cb612c4381b01d7d63a4
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-03-09 02:41:35 -0300

    ✨feat(project): create api to register in database mysql

--- node/index.js
+++ node/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app  = express();
 const port = 3000;
+const config = {
+    host: 'mysql-database',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
 
 app.get('/', (req, res) => {
     res.send('<h1>Docker node</h1>');
 });
+
+app.get('/name', (req, res) => {
+    const sql = `INSERT INTO cats(name) values('frajola')`;
+    connection.query(sql);
+    connection.end();
+    res.send(`<h1>Name: frajola, register success `)
+})
 
 app.listen(port, () => {
     console.log(`Up in port: ${port}`);
